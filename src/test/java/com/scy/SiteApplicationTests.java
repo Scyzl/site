28bcd8dce9f10113789249ee2fdcfbec2799ada9
Scyzl <1769171934@qq.com>
2020-08-06 22:06:43 +0800

    first commit：提交项目结构

---
+++ src/test/java/com/scy/SiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
